45c9a0c7eb3b94f1d3e5eca648bfe19efbff601b
ldfornari <ldfornari@gmail.com>
2022-06-09 01:41:09 -0300

    PROYECTO FINAL firebase

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,22 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyARXx_dJMPu-TIAZc6XNMbFP9ZkdjT4a9w",
-  authDomain: "ecommerce-de-fornari.firebaseapp.com",
-  projectId: "ecommerce-de-fornari",
-  storageBucket: "ecommerce-de-fornari.appspot.com",
-  messagingSenderId: "43624771582",
-  appId: "1:43624771582:web:a33a80226282e780ae1aaf"
+  apiKey: "AIzaSyDk2bp4rMw6rnvDZia3ofZoLm5ZIqTZLrg",
+  authDomain: "ecommerce-de-fornari-1.firebaseapp.com",
+  projectId: "ecommerce-de-fornari-1",
+  storageBucket: "ecommerce-de-fornari-1.appspot.com",
+  messagingSenderId: "705821751541",
+  appId: "1:705821751541:web:1fdf862ef0d67c63b82dbc"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
